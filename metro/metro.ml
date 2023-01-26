f8c108712714231c4a529c1e4e0bf1a84771766b
kuropenguin <kuro1994.t@gmail.com>
2023-01-26 22:04:22 +0000

    apply format

--- metro/metro.ml
+++ metro/metro.ml
@@ -1,23 +1,19 @@
 (* 8.5 *)
 (* 目的: 駅名のデータ(kanji, kana, romaji, shozoku)を表す型*)
 type ekimei_t =
-  {
-    kanji: string; (* 漢字 *)
-    kana: string; (* カナ *)
-    romaji: string; (*ローマ字*)
-    shozoku: string; (*路線名*)
-  }
+  { kanji: string (* 漢字 *)
+  ; kana: string (* カナ *)
+  ; romaji: string (*ローマ字*)
+  ; shozoku: string (*路線名*) }
 
 (* 8.6 *)
 (* 駅名のデータekimei_t を受け取り路線名,駅名（かな）を返す *)
 (* hyoji : ekimei_t -> string *)
-let hyoji  ekimei_t = match ekimei_t with
-  {kanji = kanji; kana = kana; shozoku=shozoku} ->
-    shozoku ^ "," ^ kanji ^ "(" ^ kana ^ ")";;
+let hyoji ekimei_t =
+  match ekimei_t with
+  | {kanji; kana; shozoku} -> shozoku ^ "," ^ kanji ^ "(" ^ kana ^ ")"
+
 (* test *)
-let test8_6_1 = hyoji {
-    kanji="茗荷谷";
-    kana="みょうがだに";
-    romaji="myougadani";
-    shozoku="丸の内線";
-  } = "丸の内線,茗荷谷(みょうがだに)"
+let test8_6_1 =
+  hyoji {kanji= "茗荷谷"; kana= "みょうがだに"; romaji= "myougadani"; shozoku= "丸の内線"}
+  = "丸の内線,茗荷谷(みょうがだに)"
