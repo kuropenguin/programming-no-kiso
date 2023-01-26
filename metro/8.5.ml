e4ce93df890250c6e5d3240b00f5d4f4f81b62be
kuropenguin <kuro1994.t@gmail.com>
2023-01-26 21:36:36 +0000

    format?????????????????????????????????????????????)

--- metro/8.5.ml
+++ metro/8.5.ml
@@ -1,3 +1,7 @@
 (* 目的: 駅名のデータ(kanji, kana, romaji, shozoku)を表す型*)
 type ekimei_t =
-  {kanji: string (* 漢字 *); kana: string (* カナ *); romaji: string (*ローマ字*)}
+  {
+    kanji: string (* 漢字 *);
+    kana: string (* カナ *); 
+    romaji: string (*ローマ字*)
+  }
