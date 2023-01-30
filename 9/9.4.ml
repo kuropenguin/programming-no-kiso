6fc73ce7ce30009df1bbbf0e2a1f69c458b203e2
kuropenguin <kuro1994.t@gmail.com>
2023-01-30 21:57:38 +0000

    fix format

--- 9/9.4.ml
+++ 9/9.4.ml
@@ -1,3 +1,5 @@
 (* 受け取ったリスト lst の長さを求める *)
 (* length : int list -> int *)
-let rec length lst = match lst with [] -> 0 | first :: rest -> 0
+let rec length lst = match lst with
+  [] -> 0
+| first :: rest ->  0
